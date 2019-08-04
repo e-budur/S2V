ea3c2577bcf49abb291721ab75dd72a82fa554ed
Emrah Budur <ebudur@levrek1.yonetim>
2019-08-04 23:22:55 +0300

    Experimented all models with XNLI dataset

--- scripts/model1_task1/run-local-eval.sh
+++ scripts/model1_task1/run-local-eval.sh
@@ -1,6 +1,6 @@
 WORK_DIR="/truba/home/ebudur/tse-s2v" #needs to be changed according to your specific environment
 
-TASK='MULTINLI-MATCHED'
+TASK='XNLI'
 
 MDLS_PATH="/truba_scratch/ebudur/data/results"
 MDL_CFGS="$WORK_DIR/model_configs"
@@ -8,7 +8,7 @@ MDL_CFGS="$WORK_DIR/model_configs"
 
 CFG="UMBC"
 
-DATA="$WORK_DIR/data/sem_sim/eng/MultiNLI_1.0/"
+DATA="$WORK_DIR/data/sem_sim/eng/XNLI-1.0/"
 
 python /truba/home/ebudur/tse-s2v/src/evaluate.py \
        --eval_task=$TASK \
